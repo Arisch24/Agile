c2539b52564fb3a0d23b7096778597a70766a412
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-07-06 09:48:43 +0800

    Feature: default page.php template

--- page.php
+++ page.php
@@ -9,27 +9,25 @@
 get_header();
 ?>
 
-<section class="ag-section">
-	<div class="ag-container">
-		<h1><?php echo esc_html( get_the_title() ); ?></h1>
-	</div>
-</section>
+<article class="ag-article">
+	<section class="ag-section">
+		<div class="ag-container">
+		<?php 
+		if ( have_posts() ) :
+			echo '<ul class="post-grid">';
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'template-parts/content', get_post_type() );
+				endwhile;
+			wp_reset_postdata();
+			echo '</ul>';
+		else :
+			get_template_part( 'template-parts/content', 'none' );
+		endif;
+		?>
+		</div>
+	</section>
+</article>
 
-<?php
-$query = new WP_Query(
-	array(
-		'post_type'      => 'post',
-		'posts_per_page' => 5,
-		'paged'          => get_query_var( 'paged' ),
-	) 
-);
-if ( $query->have_posts() ) :
-	while ( $query->have_posts() ) :
-		$query->the_post();
-		get_template_part( 'template-parts/content', 'page' );
-	endwhile;
-else :
-	get_template_part( 'template-parts/content', 'none' );
-endif;
-
+<?
 get_footer();
