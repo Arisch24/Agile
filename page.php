2c3a2bfaa776c7dcfce1d24e3340e2ccde00a104
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-07-07 00:43:46 +0800

    Feature: add comments feature to pages

--- page.php
+++ page.php
@@ -17,6 +17,11 @@ get_header();
 				while ( have_posts() ) :
 					the_post();
 					get_template_part( 'template-parts/content', get_post_type() );
+
+					// Check if comments are open or we have at least one comment.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 				endwhile;
 				wp_reset_postdata();
 			endif;
