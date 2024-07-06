8e6bc561ec5dd7b25e82749d9d92fd0697ed2914
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-07-06 09:54:09 +0800

    Fix: semanticness and functions

--- index.php
+++ index.php
@@ -10,23 +10,23 @@ get_header();
 
 ?>
 
-<section class="ag-section">
-	<div class="ag-container">
-		<?php 
-		if ( have_posts() ) :
-			echo '<ul class="post-grid">';
-			while ( have_posts() ) :
-				the_post();
-				get_template_part( 'template-parts/content', get_post_type() );
-				endwhile;
-			wp_reset_postdata();
-			echo '</ul>';
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-		endif;
-		?>
-</div>
-</section>
+<article class="ag-article">
+	<section class="ag-section">
+		<div class="ag-container">
+			<?php 
+			if ( have_posts() ) :
+				while ( have_posts() ) :
+					the_post();
+					get_template_part( 'template-parts/content', get_post_type() );
+					endwhile;
+				wp_reset_postdata();
+			else :
+				get_template_part( 'template-parts/content', 'none' );
+			endif;
+			?>
+	</div>
+	</section>
+</article>
 
 <?php
 
--- page.php
+++ page.php
@@ -14,15 +14,11 @@ get_header();
 		<div class="ag-container">
 		<?php 
 		if ( have_posts() ) :
-			echo '<ul class="post-grid">';
 			while ( have_posts() ) :
 				the_post();
 				get_template_part( 'template-parts/content', get_post_type() );
 				endwhile;
 			wp_reset_postdata();
-			echo '</ul>';
-		else :
-			get_template_part( 'template-parts/content', 'none' );
 		endif;
 		?>
 		</div>
