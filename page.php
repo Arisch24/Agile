3e3c095105b26fba33cea125a9b663237f865b81
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-07-06 09:56:40 +0800

    Styles: tab spacing

--- page.php
+++ page.php
@@ -12,15 +12,15 @@ get_header();
 <article class="ag-article">
 	<section class="ag-section">
 		<div class="ag-container">
-		<?php 
-		if ( have_posts() ) :
-			while ( have_posts() ) :
-				the_post();
-				get_template_part( 'template-parts/content', get_post_type() );
-			endwhile;
-			wp_reset_postdata();
-		endif;
-		?>
+			<?php 
+			if ( have_posts() ) :
+				while ( have_posts() ) :
+					the_post();
+					get_template_part( 'template-parts/content', get_post_type() );
+				endwhile;
+				wp_reset_postdata();
+			endif;
+			?>
 		</div>
 	</section>
 </article>
