c3b3719011e45199e67f7f918c5dc3124ff905c6
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-07-06 09:48:30 +0800

    Feature: default index.php

--- index.php
+++ index.php
@@ -8,13 +8,26 @@
 
 get_header();
 
-if ( have_posts() ) :
-	while ( have_posts() ) :
-		the_post();
-		get_template_part( 'template-parts/content', get_post_format() );
-	endwhile;
-else :
-	get_template_part( 'template-parts/content', 'none' );
-endif;
+?>
 
-	get_footer();
+<section class="ag-section">
+	<div class="ag-container">
+		<?php 
+		if ( have_posts() ) :
+			echo '<ul class="post-grid">';
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'template-parts/content', get_post_type() );
+				endwhile;
+			wp_reset_postdata();
+			echo '</ul>';
+		else :
+			get_template_part( 'template-parts/content', 'none' );
+		endif;
+		?>
+</div>
+</section>
+
+<?php
+
+get_footer();
