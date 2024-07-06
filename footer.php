6e7aca24b1a4f7b3e15d2e224072d71042e924af
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-07-06 10:33:30 +0800

    Feature: footer styles

--- footer.php
+++ footer.php
@@ -16,8 +16,8 @@ do_action( 'ag_after_main_inner' );
 <?php do_action( 'ag_before_footer' ); ?>
 
 <footer id="ag-footer" class="ag-site-footer">
-	<div class="ag-container">
-		<p>&copy; <?php echo esc_html( date_i18n( 'Y' ) ); ?> - <?php echo get_bloginfo( 'name' ); //phpcs:ignore ?></p>
+	<div class="ag-container ag-site-footer__container">
+		<small>&copy; <?php echo esc_html( date_i18n( 'Y' ) ); ?> - <?php echo get_bloginfo( 'name' ); //phpcs:ignore ?> &dot; <?php printf( esc_html__( 'Built by %1$s', 'agile' ), '<a href="https://arischvaran.com/" target="_blank">Arischvaran</a>' ); ?></small>
 	</div>
 </footer>
 
