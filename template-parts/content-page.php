8249c6d276e46e306dbf81e27febf4c8bfe205e0
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-07-06 09:50:54 +0800

    Feature: template-part for page.

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -8,13 +8,9 @@
 
 ?>
 
-<section class="ag-section">
-	<div class="ag-container">
-		<h3><?php echo esc_html( get_the_title() ); ?></h3>
-		<div class="ag-post-content">
-			<?php
-			the_content();
-			?>
-		</div>
-	</div>
-</section>
+<h1><?php echo esc_html( get_the_title() ); ?></h1>
+<div class="ag-post-content">
+	<?php
+	the_content();
+	?>
+</div>
