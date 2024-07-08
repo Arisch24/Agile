25edfd0c855c51949c3ff1e98aa2edd94d271e3a
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-07-08 21:38:00 +0800

    Build: styleignore doesn't work; too lazy to check

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,20 @@
 module.exports = {
     extends: "@wordpress/stylelint-config/scss",
     rules: {
-        "selector-class-pattern": null
-    }
+        "selector-class-pattern": null,
+        "rule-empty-line-before": null,
+        "at-rule-empty-line-before": null,
+        "scss/no-global-function-names": null,
+        "max-line-length": 120,
+        "function-comma-space-after": "always-single-line",
+        "function-parentheses-space-inside": "never-single-line",
+    },
+    ignoreFiles: [
+        "node_modules/**",
+        "build/**",
+        "dist/**",
+        "vendor/**",
+        "public/**",
+        "assets/**",
+    ],
 }
