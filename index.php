24d7c08a90c9eb22f3dda66f6b3ff36711f0f8d9
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-07-26 22:35:08 +0800

    Feature: index template

--- index.php
+++ index.php
@@ -13,13 +13,27 @@ get_header();
 <article class="ag-article">
 	<section class="ag-section">
 		<div class="ag-container">
+			<h1><?php single_post_title(); ?></h1>
+			<hr>
 			<?php 
 			if ( have_posts() ) :
+				
+				echo '<ul class="post-grid">';
+
 				while ( have_posts() ) :
 					the_post();
-					get_template_part( 'template-parts/content', get_post_type() );
-					endwhile;
-				wp_reset_postdata();
+					get_template_part( 'template-parts/content', get_post_format() );
+				endwhile;
+
+				echo '</ul>';
+
+				the_posts_pagination(
+					array(
+						'prev_text' => __( 'Previous', 'agile' ),
+						'next_text' => __( 'Next', 'agile' ),
+					)
+				);
+				
 			else :
 				get_template_part( 'template-parts/content', 'none' );
 			endif;
