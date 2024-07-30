5211dce0b90ef62f2785c4b9f9d7b46e4ca5b10c
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-07-30 21:51:26 +0800

    Feature: comment thread done

--- comments.php
+++ comments.php
@@ -1,1 +1,68 @@
 <?php
+/**
+ * The template for displaying comments.
+ *
+ * @package Agile
+ */
+
+if ( post_password_required() ) {
+	return;
+}
+?>
+
+<section class="ag-section ag-single-post__comments">
+	<div class="ag-container">
+		<?php if ( have_comments() ) : ?>
+			<h2 class="ag-single-post__comments-title">
+			<?php
+				printf(
+					esc_html( _nx( '%1$s comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'agile' ) ),
+					number_format_i18n( get_comments_number() ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+				);
+			?>
+			</h2>
+
+			<ul class="ag-single-post__comments-list">
+				<?php
+					wp_list_comments(
+						array(
+							'style' => 'ul',
+							'short_ping' => true,
+							'avatar_size' => 40,
+							'max_depth' => 5,
+							'type' => 'comment',
+							'callback' => '\Arisch\Agile\Extras\Helper_Functions::comment_callback',
+						)
+					);
+				?>
+			</ul> <!-- Comment list -->
+
+			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+				<nav class="ag-single-post__comments-nav" role="navigation">
+					<h3 class="screen-reader-text"><?php echo esc_html__( 'Comment Navigation', 'agile' ); ?></h3>
+					<div class="ag-single-post__comments-nav-prev">
+						<?php previous_comments_link( '&larr; Older Comments', 'agile' ); ?>
+					</div>
+					<div class="ag-single-post__comments-nav-next">
+						<?php next_comments_link( '&larr; Newer Comments', 'agile' ); ?>
+					</div>
+				</nav>
+			<?php endif; ?> <!-- Check for comment navigation through pagination -->
+
+			<?php if ( ! comments_open() && get_comments_number() ) : ?>
+				<p class="ag-single-post__comments-closed"><?php echo esc_html__( 'Comments are closed.', 'agile' ); ?></p>
+			<?php endif; ?> <!-- Comment form -->
+
+		<?php endif; // Have comments ?>
+
+		<?php
+		$ag_comment_args = array(
+			'title_reply_before' => '<h2 class="ag-single-post__comments-form-title">',
+			'title_reply_after' => '</h2>',
+		);
+		comment_form( $ag_comment_args );
+		?>
+
+</div>
+</section>
--- includes/Extras/Helper_Functions.php
+++ includes/Extras/Helper_Functions.php
@@ -57,4 +57,82 @@ class Helper_Functions {
 		);
 		return $comment_output;
 	}
+
+	/**
+	 * Comment callback.
+	 *
+	 * @param object $comment Comment object.
+	 * @param array  $args    Arguments.
+	 * @param int    $depth   Depth.
+	 * @return void
+	 */
+	public static function comment_callback( $comment, $args, $depth ) {
+		if ( 'div' === $args['style'] ) {
+			$tag = 'div';
+			$add_below = 'comment';
+		} else {
+			$tag = 'li';
+			$add_below = 'div-comment';
+		}
+
+		$classes = ' ' . comment_class( empty( $args['has_children'] ) ? '' : 'parent', null, null, false );
+		?>
+
+		<<?php echo $tag . $classes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?> id="comment-<?php comment_ID(); ?>">
+		<?php if ( 'div' !== $args['style'] ) { ?>
+			<div id="div-comment<?php comment_ID(); ?>" class="comment-body">
+				<?php } ?>
+				<div class="comment-author">
+					<?php
+					if ( $args['avatar_size'] !== 0 ) {
+						echo get_avatar( $comment, $args['avatar_size'] );
+					}
+					?>
+				</div>
+				<div class="comment-content">
+					<div class="comment-author vcard">
+						<h4 class="fn"><?php comment_author_link(); ?></h4>
+						<div class="comment-meta">
+							<a class="comment-meta-datetime" href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>">
+								<?php
+								printf(
+									esc_html__( '%1$s at %2$s' ),
+									esc_html( get_comment_date() ),
+									esc_html( get_comment_time() )
+								);
+								?>
+							</a>
+							<?php edit_comment_link( __( '(Edit)' ), '  ', '' ); ?>
+						</div>
+					</div>
+					<div class="comment-text">
+						<?php comment_text(); ?>
+					</div>
+					<div class="comment-reply">
+						<?php
+						comment_reply_link(
+							array_merge(
+								$args,
+								array(
+									'add_below' => $add_below,
+									'depth' => $depth,
+									'max_depth' => $args['max_depth'],
+								)
+							)
+						);
+						?>
+					</div>
+				</div>
+
+				<?php if ( $comment->comment_approved === '0' ) { ?>
+					<em class="comment-awaiting-moderation">
+						<?php echo esc_html__( 'Your comment is awaiting moderation.' ); ?>
+					</em><br />
+				<?php } ?>
+
+		<?php if ( 'div' !== $args['style'] ) { ?>
+			</div>
+			<?php
+		}
+	}
 }
