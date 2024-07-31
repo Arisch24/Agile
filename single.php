784d46242d1770aeffdb81434a1ba321c6006609
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-07-31 23:02:05 +0800

    Fix: post nav at the bottom

--- single.php
+++ single.php
@@ -17,32 +17,29 @@ get_header();
 
 		get_template_part( 'template-parts/content-single', get_post_type() );
 
-		?>
-		<section class="ag-section">
-			<div class="ag-container">
-				<?php
-					the_post_navigation(
-						array(
-							'prev_text' => '&larr;<span class="ag-single-post__prev-btn">' . __( 'Previous Post', 'agile' ) . '</span>',
-							'next_text' => '&rarr;<span class="ag-single-post__next-btn">' . __( 'Next Post', 'agile' ) . '</span>',
-							'in_same_term' => true,
-							'screen_reader_text' => __( 'Continue Reading', 'agile' ),
-						)
-					);
-				?>
-			</div>
-		</section>
-
-		<?php
 		// If comments are open or we have at least one comment, load up the comment template.
 		if ( comments_open() || get_comments_number() ) :
 			comments_template();
 		endif;
 
 	endwhile;
+
 	?>
+	<section class="ag-section ag-single-post__postnav">
+		<div class="ag-container">
+			<?php
+				the_post_navigation(
+					array(
+						'prev_text' => '&larr;<span class="ag-single-post__postnav-prev-btn">' . __( 'Previous Post', 'agile' ) . '</span>',
+						'next_text' => '&rarr;<span class="ag-single-post__postnav-next-btn">' . __( 'Next Post', 'agile' ) . '</span>',
+						'in_same_term' => true,
+						'screen_reader_text' => __( 'Continue Reading', 'agile' ),
+					)
+				);
+				?>
+		</div>
+	</section>
 </article>
 
 <?php
-
 get_footer();
