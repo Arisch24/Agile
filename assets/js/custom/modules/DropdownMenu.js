a05d7e3870ca251b6511b52550e575afefe25a9d
Arischvaran Puvanesvaran <arischvaran00@gmail.com>
2024-08-02 20:47:43 +0800

    Fix: solved hover issue

--- assets/js/custom/modules/DropdownMenu.js
+++ assets/js/custom/modules/DropdownMenu.js
@@ -1,41 +1,62 @@
 class DropdownMenu {
 	constructor() {
-		this.dropdownListElement = document.querySelectorAll( '.ag-site-header__navlist-item.has--dropdown' );
+
+		// Selectors
 		this.dropdownClassSelector = 'dropdown--active';
 		this.dropdownBtnSelector = '[data-dropdown-toggle]';
 		this.dropdownMenuSelector = '.ag-site-header__navlist-item-dropdown-submenu';
 
-		this.dropdownListElement.forEach( ( el ) => {
-			el.addEventListener( 'mouseover', ( e ) => this.toggleDropdown ( el, e ) );
-			el.addEventListener( 'mouseout', ( e ) => this.toggleDropdown ( el, e ) );
-			el.addEventListener( 'keydown', ( e ) => this.handleKeyPress( el, e ) );
+		// Query Selector All Items
+		this.dropdownListArr = document.querySelectorAll( '.ag-site-header__navlist-item.has--dropdown' );
+		this.dropdownBtnArr = document.querySelectorAll( this.dropdownBtnSelector );
+
+		this.init();
+	}
+
+	init() {
+		this.dropdownListArr.forEach( ( listItem ) => {
+			listItem.addEventListener( 'mouseenter', ( e ) => this.addDropdown( listItem, e ) );
+			listItem.addEventListener( 'mouseleave', ( e ) => this.removeDropdown( listItem, e ) );
+		});
+
+		this.dropdownBtnArr.forEach( ( button ) => {
+			button.addEventListener( 'keydown', ( e ) => this.handleKeyPress( button, e ) );
+
+			// button.addEventListener( 'focusin', ( e ) => this.addDropdown( button, e ) );
+			button.addEventListener( 'focusout', ( e ) => this.removeDropdown( button, e ) );
 		});
 	}
 
-	toggleDropdown( el, e ) {
-		const dropdownBtn = el.querySelector( this.dropdownBtnSelector );
-		const dropdownMenu = el.querySelector( this.dropdownMenuSelector );
-		const isDropdownActive = dropdownMenu.classList.contains( this.dropdownClassSelector );
+	addDropdown( listItem ) {
+		const dropdownBtn = listItem.querySelector( this.dropdownBtnSelector );
+		const dropdownMenu = listItem.querySelector( this.dropdownMenuSelector );
 
-		if ( e.type === 'mouseover' ) {
-			dropdownMenu.classList.add( this.dropdownClassSelector );
-		}
-		if ( e.type === 'mouseout' ) {
-			dropdownMenu.classList.remove( this.dropdownClassSelector );
-		}
-		if ( e.type === 'keydown' ) {
-			isDropdownActive ? dropdownMenu.classList.remove( this.dropdownClassSelector ) : dropdownMenu.classList.add( this.dropdownClassSelector );
-		}
+		dropdownMenu.classList.add( this.dropdownClassSelector );
 
-		dropdownBtn.setAttribute( 'data-dropdown-toggle', isDropdownActive );
-		dropdownBtn.setAttribute( 'aria-expanded', isDropdownActive );
+		this.setAttributes( dropdownBtn, true );
 	}
 
-	handleKeyPress( el, event ) {
-		if ( event.keyCode !== 32 && event.keyCode !== 13 ) {
+	removeDropdown( listItem ) {
+		const dropdownBtn = listItem.querySelector( this.dropdownBtnSelector );
+		const dropdownMenu = listItem.querySelector( this.dropdownMenuSelector );
+
+		dropdownMenu.classList.remove( this.dropdownClassSelector );
+
+		this.setAttributes( dropdownBtn, false );
+	}
+
+	setAttributes( btn, dataAttr ) {
+		btn.setAttribute( 'data-dropdown-toggle', dataAttr );
+		btn.setAttribute( 'aria-expanded', dataAttr );
+	}
+
+	handleKeyPress( button, event ) {
+		if ( event.keyCode !== 32 && event.keyCode !== 13 && event.keyCode !== 27 ) {
 			return;
 		}
-		this.toggleDropdown( el, event );
+		const parentEl = button.parentNode.parentNode;
+
+		button.parentNode.nextElementSibling.classList.contains( this.dropdownClassSelector ) ? this.removeDropdown( parentEl ) : this.addDropdown( parentEl );
 	}
 }
 
